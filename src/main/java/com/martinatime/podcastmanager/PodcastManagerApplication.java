a36aad9aee30d58c486acc3bc4da1a843b1c797f
Aaron Martina <martinatime@gmail.com>
2023-12-15 18:31:35 -0500

    Initial commit

---
+++ src/main/java/com/martinatime/podcastmanager/PodcastManagerApplication.java
@@ -0,0 +1,13 @@
+package com.martinatime.podcastmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martinatime/podcastmanager/PodcastManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinatime.podcastmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
